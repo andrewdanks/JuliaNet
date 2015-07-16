9b4a169b36f5f5df08a1cd026168b86679d1a36c
danks <a.danks@gmail.com>
2015-07-15 19:57:57 -0700

    moving new dropout code to regularization

--- src/regularization.jl
+++ src/regularization.jl
@@ -1,7 +1,24 @@
-function L1(nn::NeuralNetwork)
-    sum(abs(nn.layers[end].weights))
+function set_dropout_masks!(layers::Vector{LinkedLayer}, batch_size::T_INT)
+    for layer in layers
+        if isdefined(layer.data_layer, :dropout_coefficient)
+            set_dropout_mask!(layer, layer.data_layer.dropout_coefficient, batch_size)
+        end
+    end
 end
 
-function L2(nn::NeuralNetwork)
-    sum(nn.layers[end].weights .^ 2)
+
+function set_dropout_mask!(layer::LinkedLayer, coefficient::T_FLOAT, batch_size::T_INT)
+    mask_size = (size(layer.data_layer.weights)[2], batch_size)
+    layer.dropout_mask = get_dropout_mask(
+        mask_size,
+        coefficient
+    )
+end
+
+
+function get_dropout_mask(size, prob)
+    M = rand(size)
+    M[find(x -> x < prob, M)] = 0.
+    M[find(x -> x != 0, M)] = 1.
+    M
 end
