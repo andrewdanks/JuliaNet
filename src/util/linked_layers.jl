2b2c2a9bde94ce1a2703b35cdf6f7ae76dce7c10
danks <a.danks@gmail.com>
2015-07-18 12:37:38 -0700

    rename train.jl -> linked_layers.jl

--- src/util/linked_layers.jl
+++ src/util/linked_layers.jl
@@ -1,5 +1,5 @@
-function get_linked_layers(layers::Vector{NeuralLayer})
-    linked_layers = [LinkedLayer(layer) for layer in layers]
+function get_linked_layers{T<:NeuralLayer}(layers::Vector{T})
+    linked_layers = [LinkedLayer{typeof(layer)}(layer) for layer in layers]
 
     linked_layers[1].next = linked_layers[2]
     for i = 2:length(linked_layers)-1
@@ -19,7 +19,10 @@ function get_linked_layers(
     linked_layers = get_linked_layers(layers)
     for (i, prev_ll) in enumerate(prev_linked_layers)
         layer = linked_layers[i]
-        layer.prev_weight_delta = prev_ll.weight_delta
+        if isdefined(prev_ll, :weight_delta)
+            layer.prev_weight_delta = prev_ll.weight_delta
+        end
     end
+
     linked_layers
 end
