a8b50ea8fccb7ce1317b2641022eded869ed1d65
danks <a.danks@gmail.com>
2015-07-18 13:34:43 -0700

    move neural layer computations out of train.jl

--- src/train.jl
+++ src/train.jl
@@ -176,22 +176,6 @@ function fit_batch(
 end
 
 
-function update_weights!{T<:NeuralLayer}(layer::LinkedLayer{T}, params::HyperParams)
-    prev_weight_delta = 0
-    if params.momentum > 0 && isdefined(layer, :prev_weight_delta)
-        prev_weight_delta = layer.prev_weight_delta 
-    end
-    weight_delta = params.momentum * prev_weight_delta - params.learning_rate * layer.grad_weights
-    layer.weight_delta = weight_delta
-    layer.data_layer.weights += weight_delta
-end
-
-
-function get_pre_activation(layer::NeuralLayer, input::InputTensor)
-    layer.weights' * vectorized_data(input)
-end
-
-
 function forward_pass!(
     input::InputTensor,
     layer::LinkedLayer
@@ -243,22 +227,3 @@ function backward_pass!(
     end
 end
 
-
-function get_grad_weights{T<:NeuralLayer}(
-    layer::LinkedLayer{T},
-    error_signal::T_2D_TENSOR
-)
-    batch_size = size(error_signal)[2]
-    vectorized_data(layer.input) * error_signal' / batch_size
-end
-
-
-function get_grad_error_wrt_net{T<:NeuralLayer}(
-    layer::LinkedLayer{T},
-    error_signal::T_2D_TENSOR
-)
-    grad_activation_fn = layer.prev.data_layer.activator.grad_activation_fn
-    weights = layer.data_layer.weights
-    prev_pre_activation = vectorized_data(InputTensor(layer.prev.pre_activation))
-    (weights * error_signal) .* grad_activation_fn(prev_pre_activation)
-end
