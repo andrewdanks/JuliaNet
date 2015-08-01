1121e0642be495b87db2243cfa00f3a53937d7da
danks <a.danks@gmail.com>
2015-07-31 23:42:10 -0700

    no batch size in hyper params

--- src/type/HyperParams.jl
+++ src/type/HyperParams.jl
@@ -2,7 +2,6 @@ type HyperParams
     learning_rate::T_FLOAT
     momentum::T_FLOAT
     epochs::T_INT
-    batch_size::T_INT
     stop_criterion_fn::Function
     loss_fn::Function
 
@@ -11,10 +10,6 @@ type HyperParams
         # Number of times to sweep through the data
         epochs::T_UINT=1,
 
-        # How much data to process before each weight update
-        # 1 = online learning, Inf=full batch
-        batch_size::T_UINT=1
-
         ;
 
         momentum::T_FLOAT=0.,
@@ -22,16 +17,12 @@ type HyperParams
         # Automatically stop training based on some criterion
         stop_criterion_fn::Function=no_stop_criterion_fn,
 
-        loss_fn::Function=mean_squared_error
+        loss_fn::Function=cross_entropy_error
     )
-
-        assert(batch_size > 0)
-
         new(
             learning_rate,
             momentum,
             epochs,
-            batch_size,
             stop_criterion_fn,
             loss_fn
         )
@@ -42,6 +33,5 @@ function Base.show(io::IO, params::HyperParams)
     println(io, "Learning Rate: ", params.learning_rate)
     println(io, "Momentum: ", params.momentum)
     println(io, "Epochs: ", params.epochs)
-    println(io, "Batch Size: ", params.batch_size)
     println(io, "Stop Criterion: ", params.stop_criterion_fn)
 end
